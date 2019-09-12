5f7ed37a0e6234699345d019023ac031847ab876
hulahulahhh <1002340239@qq.com>
2019-09-12 17:18:57 +0800

    feat: 增加then方法链式调用

--- promise.js
+++ promise.js
@@ -1,49 +1,78 @@
-const PENDING = 'PENDING'
-const FULLFILLED = 'FULLFILLED'
-const REJECTED = 'REJECTED'
+const PENDING = "PENDING";
+const FULLFILLED = "FULLFILLED";
+const REJECTED = "REJECTED";
 
 class MyPromise {
-    constructor(executor) {
-        if (typeof executor !== 'function') {
-            throw 
-        }
-        this.$state = PENDING
-        this.$chained = []
-        const resolve = (res) => {
-            if (this.$state !== PENDING) {
-                return;
-            }
-            this.$state = FULLFILLED
-            this.$internalValue = res
-            for (const { onFulfilled } of this.$chained) {
-                onFulfilled(res);
-            }
-        }
-        const reject = (res) => {
-            if (this.$state !== PENDING) {
-                return;
-            }
-            this.$state = REJECTED
-            this.$internalValue = res
-            for (const { onRejected } of this.$chained) {
-                onRejected(err);
-            }
-        }
+  constructor(executor) {
+    if (typeof executor !== "function") {
+      throw new Error("executor must be a function");
+    }
+    this.$state = PENDING;
+    this.$chained = [];
+    const resolve = res => {
+      if (this.$state !== PENDING) {
+        return;
+      }
+      this.$state = FULLFILLED;
+      this.$internalValue = res;
+      for (const { onFulfilled } of this.$chained) {
+        onFulfilled(res);
+      }
+    };
+    const reject = res => {
+      if (this.$state !== PENDING) {
+        return;
+      }
+      this.$state = REJECTED;
+      this.$internalValue = res;
+      for (const { onRejected } of this.$chained) {
+        onRejected(err);
+      }
+    };
 
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  then(onFullfilled, onRejected) {
+    return new MyPromise((resolve, reject) => {
+      const _onRejected = err => {
         try {
-            executor(resolve, reject)  
-        } catch (error) {
-            reject(error)
+          reject(onRejected(err));
+        } catch (_err) {
+          reject(_err);
         }
-    }
-
-    then(onFullfilled, onRejected) {
-        if (this.$state === PENDING) {
-            this.$chained.push({onFullfilled, onRejected})
-        } else if (this.$state === REJECTED) {
-            onRejected(this.$internalValue)
-        } else {
-            onFullfilled(this.$internalValue)
+      };
+      const _onFullfilled = res => {
+        try {
+          resolve(onFullfilled(this.$internalValue));
+        } catch (err) {
+          reject(err);
         }
-    }
+      };
+      if (this.$state === PENDING) {
+        this.$chained.push({ onFullfilled, onRejected });
+      } else if (this.$state === REJECTED) {
+        _onRejected(this.$internalValue);
+      } else {
+        _onFullfilled(this.$internalValue);
+      }
+    });
+  }
 }
+
+const p = new MyPromise((resolve, reject) => {
+  resolve(1);
+});
+
+p.then(res => {
+  console.log(res);
+  return new MyPromise((resolve, reject) => {
+    resolve("new");
+  });
+}).then(res => {
+  console.log(res);
+});
